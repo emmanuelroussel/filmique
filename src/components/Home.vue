2fe2e01db60c6f69a6661d370b88ac2815d6cc59
Emmanuel Roussel <emman.roussel@gmail.com>
2016-12-09 11:19:38 -0500

    Replace input placeholder

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1 class="logo logo-full-size">filmique</h1>
     <h4>Search for any theme and get a list of films corresponding to that theme</h4>
-    <input class="input-home" type="text" placeholder="Search" />
+    <input class="input-home" type="text" placeholder="Ex: Space, World War 2, Gangster, etc." />
     <button class="button-primary">Find films</button>
   </div>
 </template>
