d77257e882f6a6cba675c4de956272475f6c49ee
Emmanuel Roussel <emman.roussel@gmail.com>
2016-12-12 12:54:24 -0500

    Add SearchResult component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,112 @@
 <template>
   <div id="app">
     <home v-on:search="search"></home>
+    <search-result v-if="results.length > 0" :movies="results" :search-input="searchInput"></search-result>
   </div>
 </template>
 
 <script>
 import Home from './components/Home'
+import SearchResult from './components/SearchResult'
 
 export default {
   name: 'app',
   components: {
-    Home
+    Home,
+    SearchResult
   },
   data: function () {
     return {
-      searchInput: 'Hello'
+      searchInput: '',
+      results: []
     }
   },
   methods: {
     search: function (input) {
       this.searchInput = input
+
+      // Do search
+
+      this.results = [
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        },
+        {
+          poster_path: '/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg',
+          overview: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+          release_date: '2015-09-30',
+          title: 'The Martian'
+        }
+      ]
     }
   }
 }
---
+++ src/components/SearchResult.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container movies">
+    <div class="row">
+      <h2>Results for: {{ searchInput }}</h2>
+    </div>
+    <div class="row">
+      <div class="three columns" v-for="movie in movies">
+        {{ movie.title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search-result',
+  props: ['movies', 'searchInput']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
