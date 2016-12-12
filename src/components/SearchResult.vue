2b16baeb6bd78aa493d1b3cd2947b30fe370bec4
Emmanuel Roussel <emman.roussel@gmail.com>
2016-12-12 14:42:11 -0500

    Add responsive grid

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container movies">
     <div class="row">
-      <h2>Results for: {{ searchInput }}</h2>
+      <h3>Results for: {{ searchInput }}</h3>
     </div>
-    <div class="row">
-      <div class="three columns" v-for="movie in movies">
-        {{ movie.title }}
+    <div class="row grid">
+      <div class="custom-column" v-for="movie in movies">
+        <div class="grid-inner-container">
+          <img class="u-max-full-width movie-poster" v-bind:src="movie.poster_path" />
+          <div class="movie-title">
+            {{ movie.title }}
+          </div>
+          <div>
+            {{ movie.release_date }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -20,4 +28,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.movie-title {
+  font-weight: bold;
+}
+.grid {
+  text-align: center;
+  margin-left: -1em;
+  margin-right: -1em;
+}
+.movie-poster {
+  padding-left: 1em;
+  padding-right: 1em;
+  padding-top: 1em;
+}
+/* Larger than phablet */
+@media (min-width: 550px) {
+  .grid {
+    margin-left: -2em;
+    margin-right: -2em;
+  }
+  .movie-poster {
+    padding-left: 2em;
+    padding-right: 2em;
+    padding-top: 2em;
+  }
+}
 </style>
