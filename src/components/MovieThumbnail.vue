e76059905e6505a9a9ecfd82547a04d8a2e4855a
Emmanuel Roussel <emman.roussel@gmail.com>
2016-12-20 12:53:00 -0500

    Move movie basic info in a new template

---
+++ src/components/MovieThumbnail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div >
+    <div class="movie-poster">
+      <img class="u-full-width" v-bind:src="posterPath" />
+    </div>
+    <div class="movie-title">
+      {{ title }}
+    </div>
+    <div>
+      {{ releaseDate | getYear }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'movie-thumbnail',
+  props: ['posterPath', 'title', 'releaseDate'],
+  filters: {
+    getYear: function (date) {
+      if (!date) return ''
+      return date.slice(0, date.indexOf('-'))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.movie-title {
+  font-weight: bold;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.movie-poster {
+  padding-top: 150%;
+  position: relative;
+}
+.movie-poster > img {
+  position: absolute;
+  padding-left: 1em;
+  padding-right: 1em;
+  padding-top: 2em;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+/* Larger than phablet */
+@media (min-width: 1000px) {
+  .movie-poster > img {
+    padding-left: 2em;
+    padding-right: 2em;
+    padding-top: 4em;
+  }
+}
+</style>
--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -4,31 +4,25 @@
       <h3>Results for: {{ searchInput }}</h3>
     </div>
     <div class="row grid">
-      <div class="custom-column" v-for="movie in movies">
-        <div class="grid-inner-container">
-          <div class="movie-poster">
-            <img class="u-full-width" v-bind:src="movie.poster_path" />
-          </div>
-          <div class="movie-title">
-            {{ movie.title }}
-          </div>
-          <div>
-            {{ movie.release_date | getYear }}
-          </div>
-        </div>
+      <div class="custom-column" v-for="(movie, index) in movies" v-on:click="toggleInfo(index)" v-bind:id="'movie-' + index">
+        <movie-thumbnail :poster-path="movie.poster_path" :title="movie.title" :release-date="movie.release_date"></movie-thumbnail>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import MovieThumbnail from './MovieThumbnail'
+
 export default {
   name: 'search-result',
+  components: {
+    MovieThumbnail
+  },
   props: ['movies', 'searchInput'],
-  filters: {
-    getYear: function (date) {
-      if (!date) return ''
-      return date.slice(0, date.indexOf('-'))
+  methods: {
+    toggleInfo: function (index) {
+      console.log(index)
     }
   }
 }
@@ -36,44 +30,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.movie-title {
-  font-weight: bold;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
 .grid {
   text-align: center;
   margin-left: -1em;
   margin-right: -1em;
-}
-.movie-poster {
-  padding-top: 150%;
-  position: relative;
-  margin-top: -1em;
-}
-.movie-poster > img {
-  position: absolute;
-  padding-left: 1em;
-  padding-right: 1em;
-  padding-top: 2em;
-  height: 100%;
-  left: 0;
-  top: 0;
 }
 /* Larger than phablet */
 @media (min-width: 1000px) {
   .grid {
     margin-left: -2em;
     margin-right: -2em;
-  }
-  .movie-poster {
-    margin-top: -2em;
-  }
-  .movie-poster > img {
-    padding-left: 2em;
-    padding-right: 2em;
-    padding-top: 4em;
   }
 }
 </style>
