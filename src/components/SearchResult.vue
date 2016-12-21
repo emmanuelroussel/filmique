b67c337d92b69399aadab27efc2cb77a8ac598f6
Emmanuel Roussel <emman.roussel@gmail.com>
2016-12-21 18:58:34 -0500

    Hide movie info on click on same movie

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -10,7 +10,7 @@
         </div>
       </div>
     </div>
-    <movie-info v-show="selectedMovie.show" id="movie-info" :movie="selectedMovie.info" :index="selectedMovie.index"></movie-info>
+    <movie-info v-show="selectedMovie.show" id="movie-info" :movie="selectedMovie.info" :index="selectedMovie.index % 4"></movie-info>
   </div>
 </template>
 
@@ -37,20 +37,24 @@ export default {
   },
   methods: {
     toggleInfo: function (movie, index) {
-      // Find where the movie-info component should go
-      const movieInfoIndex = (Math.ceil((index + 1) / 4.0) * 4) - 1
+      if (index === this.selectedMovie.index) {
+        this.selectedMovie.show = !this.selectedMovie.show
+      } else {
+        // Find where the movie-info component should go
+        const movieInfoIndex = (Math.ceil((index + 1) / 4.0) * 4) - 1
 
-      this.$http.get('http://localhost:3000/api/movies/' + movie.id).then(function (res) {
-        this.selectedMovie.info = res.body
-        this.selectedMovie.index = index % 4
+        this.$http.get('http://localhost:3000/api/movies/' + movie.id).then(function (res) {
+          this.selectedMovie.info = res.body
+          this.selectedMovie.index = index
 
-        // Move movie-info component to the right position in the DOM
-        $(this.$el).find('#movie-' + movieInfoIndex).append($('#movie-info'))
+          // Move movie-info component to the right position in the DOM
+          $(this.$el).find('#movie-' + movieInfoIndex).append($('#movie-info'))
 
-        this.selectedMovie.show = true
-      }, function (err) {
-        console.error(err)
-      })
+          this.selectedMovie.show = true
+        }, function (err) {
+          console.error(err)
+        })
+      }
     }
   }
 }
